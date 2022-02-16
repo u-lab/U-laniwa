594116bed274f648ad5fd2704dd2c98fded6b764
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-02-16 17:44:35 +0900

    wip シーダー作成

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(CountryTableSeeder::class);
+        $this->call(GenderTableSeeder::class);
+        $this->call(GradeTableSeeder::class);
+        $this->call(MunicipalityTableSeeder::class);
+        $this->call(Notice_genreTableSeeder::class);
+        $this->call(NoticeTableSeeder::class);
+        $this->call(PrefectureTableSeeder::class);
+        $this->call(Project_belongedTableSeeder::class);
+        $this->call(Project_participation_requestTableSeeder::class);
+        $this->call(Project_progressTableSeeder::class);
+        $this->call(ProjectTableSeeder::class);
+        $this->call(User_belonged_organizationTableSeeder::class);
+        $this->call(User_invite_codeTableSeeder::class);
+        $this->call(User_linkTableSeeder::class);
+        $this->call(User_qualificationTableSeeder::class);
+        $this->call(User_roleTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(UU_facultyTableSeeder::class);
+        $this->call(UU_majorTableSeeder::class);
     }
 }

5254cd0e9d881770c16dbb30af33b94b4c58f6b0
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-02-18 11:20:46 +0900

    wip 不要になったデータベース削除

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,23 +17,18 @@ class DatabaseSeeder extends Seeder
         $this->call(UserRoleTableSeeder::class);
         $this->call(UUFacultyTableSeeder::class);
         $this->call(UUMajorTableSeeder::class);
-        $this->call(GenderTableSeeder::class);
         $this->call(CountryTableSeeder::class);
         $this->call(PrefectureTableSeeder::class);
         $this->call(MunicipalityTableSeeder::class);
-        $this->call(GradeTableSeeder::class);
         //user依存↑
         $this->call(UserTableSeeder::class); //自動生成(40人)+手動1
         //user被依存↓
         $this->call(UserInviteCodeTableSeeder::class);
         $this->call(UserLinkTableSeeder::class); //自動生成(80)
-        $this->call(UserBelongedOrganizationTableSeeder::class); //自動生成(30)
-        $this->call(UserQualificationTableSeeder::class); //自動生成(80)
         $this->call(ProjectTableSeeder::class); //自動生成(20)+手動1
         $this->call(ProjectBelongedTableSeeder::class); //自動生成(50)
         $this->call(ProjectParticipationRequestTableSeeder::class); //自動生成(10)
         $this->call(ProjectProgressTableSeeder::class); //自動生成(60)
-        $this->call(Notice_genreTableSeeder::class);
         $this->call(NoticeTableSeeder::class); //自動生成(120)
     }
 }

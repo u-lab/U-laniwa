b83455ea730456dc94bd8eb2f0e34162ce959df0
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-02-23 18:08:31 +0900

    feat コード生成処理作成

--- backend/app/Http/Controllers/Procedure/DoRegenerateInviteCodeController.php
+++ backend/app/Http/Controllers/Procedure/DoRegenerateInviteCodeController.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Procedure;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\UserInviteCode;
+use Auth;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
+use Illuminate\Support\Str;
 
 class DoRegenerateInviteCodeController extends Controller
 {
@@ -19,6 +23,18 @@ class DoRegenerateInviteCodeController extends Controller
      */
     public function __invoke(): Redirector | RedirectResponse
     {
+        /** @var User */
+        $user = Auth::user();
+        //権限レベルが足りなければボタンは表示されないが、念の為の実装
+        if ($user->user_role_id >= 40) {
+            //無かったら作成、あれば上書きする
+            UserInviteCode::updateOrCreate([
+                'user_id' => $user->id,
+            ], [
+                'user_id'   => $user->id,
+                'code' => Str::uuid(),
+            ]);
+        }
         return redirect('/procedure');
     }
 }

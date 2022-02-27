85663206c2dace5079c6da800963ffd519cd0d25
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-02-27 18:36:19 +0900

    wip

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -114,4 +114,13 @@ return [
         ],
     ],
 
+
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ],
+
 ];

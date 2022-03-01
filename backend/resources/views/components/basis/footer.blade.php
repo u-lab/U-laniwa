9b3785dbd14c3676ef377142773484ad296c0b5a
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2022-03-01 16:11:17 +0900

    feat top-page

--- backend/resources/views/components/basis/footer.blade.php
+++ backend/resources/views/components/basis/footer.blade.php
@@ -1,6 +1,15 @@
 <!---PCフッターここから--->
-<footer class="py-4">
+<footer class="w-full flex justify-center items-center flex-col bg-bg-sub text-center text-sm" style='height: 110px'>
+    <div class="flex gap-8 justify-center mb-2">
+        <a href='/releseNote'>リリースノート</a>
+        <a href='/aboutThisSite'>このサイトについて</a>
+        <a href='/terms'>利用規約</a>
+        <a href='/privacyPolicy'>プライバシーポリシー</a>
+        <a href='/credit'>クレジット</a>
+        <a href='/contact'>お問い合わせ</a>
+    </div>
+    <a href='https://github.com/u-lab/U-laniwa' target="_blank" rel="noopener" class="mb-4">GitHub</a>
 
-    <p class="text-center mt-2 copyright">Copyright (c) {{date("Y")}} U-laniwa developer</p>
+    <p class="copyright">Copyright (c) {{date("Y")}} U-laniwa developer</p>
 </footer>
 <!---PCフッターここまで--->
--- backend/resources/views/components/buttons/loginButton.blade.php
+++ backend/resources/views/components/buttons/loginButton.blade.php
@@ -1,1 +1,1 @@
-<a href="{{$url}}" class="bg-bg inline-block p-6 w-64 rounded-2xl text-center" >{{$name}}</a>
+<a href="{{$url}}" class="bg-bg inline-block p-5 w-48 rounded-2xl text-center" >{{$name}}</a>

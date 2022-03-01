b76a11ff7c6405068191027fd3658cbeba2a68ec
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-03-01 14:10:56 +0900

    feat 都道府県、市区町村、学科を取得するAPI実装、国と学部を取得する処理を追加

---
+++ backend/app/Http/Controllers/Api/Major/GetMajorController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\Major;
+
+use App\Http\Controllers\Controller;
+use App\Models\UUMajor;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class GetMajorController extends Controller
+{
+    /**
+     *  都道府県コードから該当する市区町村のデータを入れたjsonファイルを返すController
+     *
+     * @return JsonResponse
+     */
+    public function __invoke(int $facultyId): JsonResponse
+    {
+        $majors = UUMajor::where('facultyId', $facultyId)->get();
+        return  response()->json($majors);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\Area\GetCountryController;
+use App\Http\Controllers\Api\Area\GetMunicipalityController;
+use App\Http\Controllers\Api\Area\GetPrefectureController;
+use App\Http\Controllers\Api\Major\GetMajorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('/get/area/prefecture/{country_id}', GetPrefectureController::class)->name('getPrefecture');
+Route::get('/get/area/municipality/{prefecture_id}', GetMunicipalityController::class)->name('getMunicipality');
+Route::get('/get/major/{faculty_id}', GetMajorController::class)->name('getMajor');
+Route::middleware(['auth:sanctum', 'verified', 'first'])->group(function () {
 });

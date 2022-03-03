a7a42096d4e33bc343b28b7ea3b720e436555f1d
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-03-03 22:40:04 +0900

    feat ユーザー一覧での情報精度改善

--- backend/app/Http/Controllers/User/ShowAllUserController.php
+++ backend/app/Http/Controllers/User/ShowAllUserController.php
@@ -34,16 +34,40 @@ class ShowAllUserController extends Controller
         $users = DB::table('user_infos')
             ->join('users', 'user_infos.user_id', '=', 'users.id')
             ->leftJoin('u_u_majors', 'user_infos.u_u_major_id', '=', 'u_u_majors.id')
-            ->select('users.id', 'users.name', 'users.profile_photo_path', 'user_infos.status', 'user_infos.last_name', 'user_infos.first_name', 'user_infos.grade', 'u_u_majors.name as uu_major', 'u_u_majors.faculty_id as uu_faculty')
+            ->select('users.id', 'users.name', 'users.profile_photo_path',   'user_infos.company_meta', 'user_infos.university_meta', 'user_infos.last_name', 'user_infos.first_name', 'user_infos.grade', 'u_u_majors.name as uu_major', 'u_u_majors.faculty_id as uu_faculty')
             ->orderBy('grade', 'asc') //後ほど学部順で使うため
             ->get();
 
+
         $listedUsers = [];
         foreach ($users as $user) {
+
+
+
             /**
-             * enmuを用いて学部を取得
+             * 社会人→会社名＋役職
+             * 他大生→学名＋学部＋学科
+             * 宇大生→学部＋学科
+             * (宇大生から社会人になった場合、宇大のデータが残っている可能性が否めないので、それ防止のために先に社会人から条件分岐させている)
              */
-            $user->uu_faculty = $user->uu_faculty == null ? "" : UUFaculty::from($user->uu_faculty)->label();
+
+            if ($user->company_meta) {
+                $company_meta = json_decode($user->company_meta);
+                $user->profession = $company_meta->company_name . " " . $company_meta->position;
+            } else if ($user->university_meta) {
+                $university_meta = json_decode($user->university_meta);
+                $user->profession = $university_meta->university . " " . $university_meta->faculty . " " . $university_meta->major;
+            } else if ($user->uu_faculty) {
+                /**
+                 * enmuを用いて学部を取得
+                 */
+                $user->uu_faculty =  UUFaculty::from($user->uu_faculty)->label();
+                $user->profession = $user->uu_faculty . " " . $user->uu_major;
+            } else {
+                //この場合は存在しないはずだがエラーになるのは困るので空文字
+                $user->profession = '';
+            }
+
 
             /**
              * 学年ごとに仕分けする

09ea93353ec58e000100ab8f5e8537af8fd6eb1e
SeigoMori <85787242+SeigoMori@users.noreply.github.com>
2022-03-05 00:41:18 +0900

    feat 招待ユーザーのセクションを作成

--- backend/app/Http/Controllers/Procedure/ShowProcedureController.php
+++ backend/app/Http/Controllers/Procedure/ShowProcedureController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Procedure;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -23,10 +25,15 @@ class ShowProcedureController extends Controller
      */
     public function __invoke(): View|Factory
     {
+        $userId = Auth::id();
+        $invitedUsers = User::where('invited_id', $userId)
+            ->orderBy('created_at', 'desc')
+            ->take(20)
+            ->get();
         /**
          * https://github.com/u-lab/U-laniwa/wiki/507_%E8%AA%8D%E5%8F%AF%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6
          * 認可をフロントで用いることをおすすめします。。
          */
-        return view('procedure.index', ['gate' => $this->gate,]);
+        return view('procedure.index', ['gate' => $this->gate, 'invitedUsers' => $invitedUsers]);
     }
 }
--- backend/resources/views/procedure/index.blade.php
+++ backend/resources/views/procedure/index.blade.php
@@ -8,4 +8,13 @@
 <h1 class="text-center my-8 text-3xl ">手続き</h1>
 
 
+
+<h2>招待ユーザー</h2>
+@foreach ($invitedUsers as $invitedUser)
+<img src="{{url('/' . $invitedUser->profile_photo_path)}}" alt="">
+<p>{{$invitedUser->name}}</p>
+{{-- この値で表示数を変更(1件~20件) --}}
+@break ($loop->iteration == 3)
+@endforeach
+
 @endsection

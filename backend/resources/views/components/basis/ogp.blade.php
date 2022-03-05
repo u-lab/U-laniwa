726aae0572d5a1740650b48e644d955de0762ecf
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-03-05 15:34:41 +0900

    feat OGP設定

--- backend/resources/views/components/basis/ogp.blade.php
+++ backend/resources/views/components/basis/ogp.blade.php
@@ -2,7 +2,7 @@
 <meta property="og:title" content="@yield('title') | U-laniwa">
 <meta property="og:type" content="website">
 <meta property="og:url" content="https://u-laniwa.tk/">
-<meta property="og:image" content="https://u-laniwa.tk/ogp.png">
+<meta property="og:image" content="https://u-laniwa.tk/img/ulaniwa_ogp.png">
 <meta property="og:site_name" content="U-laniwa">
 <meta property="og:description" content="U-lab向け内部システムです">
 <meta name="twitter:card" content="summary">

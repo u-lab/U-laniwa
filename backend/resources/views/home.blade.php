7077c9040cbdc97c6dd966bd7df56e5036e9dffb
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2022-03-05 23:45:13 +0900

    feat /home

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -27,27 +27,25 @@
 
         </div>
     </div>
-    @if($userProjects->isEmpty())
-    <h2 class="text-2xl">
-        現在進行中のプロジェクトはありません
-    </h2>
-    @else
-    <h2 class="text-2xl">
-        現在進行中のプロジェクトは </h2>
-    @foreach ($userProjects as $userProject)
-    <a href="{{url('/project/'.$userProject->id)}}">
-        <img src="{{url('/'.$userProject->thumbnail)}}" alt="">
-        <p>タイトル:{{$userProject->title}} </p>
-        <p>サブタイトル:{{$userProject->subtitle}} </p>
-        <p>
-            {{$userProject->start_date}}
-            @empty($userProject->end_date)
-            @else
-            ～{{$userProject->end_date}}
-            @endempty
-        </p>
-    </a>
-    @endforeach
-    @endif
+
+    <div class="basicInformation h-fit p-6 border-4 border-bg rounded-2xl relative mb-16">
+        <h2 class="absolute py-2 px-6 bg-bg rounded-full text-base font-bold" style="top: -1.125rem;">現在進行中のプロジェクト</h2>
+        @if($userProjects->isEmpty())
+        <p class="py-6 text-center">現在進行中のプロジェクトはありません。</p>
+        @else
+        <div class=" flex flex-wrap gap-x-12 my-8">
+            @foreach($userProjects as $userProject)
+            <a href="{{url('/project/' . $userProject->project_id)}}" target="_blank" rel="noopener"
+                class="bg-bg-sub text-center rounded-2xl p-6 hover:opacity-80 userFrame"
+                style="transition: .2s; width: 500px">
+                <img src="{{url('/' . $userProject->thumbnail)}}" alt="" class="rounded mb-8">
+                <h3 class="ml-4 text-xl font-bold mb-2">{{$userProject->title}}</h3>
+                <p class="mb-2">{{$userProject->subtitle}}</p>
+                <p class="text-sm mb-4">{{$userProject->start_date}} 〜 {{$userProject->end_date}}</p>
+            </a>
+            @endforeach
+        </div>
+        @endif
+    </div>
 </div>
 @endsection

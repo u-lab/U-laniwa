91d04af461331d896c8bfc90de37e303418219fe
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2022-03-06 22:04:00 +0900

    feat responsive-security/procedure

--- backend/resources/views/security/index.blade.php
+++ backend/resources/views/security/index.blade.php
@@ -8,14 +8,14 @@
 @include('components.forMembers.pageTitle', ['title'=>'セキュリティ'])
 {{-- inputタグのvalueに入れておく用 --}}
 @include('components.forMembers.pageSubTitle', ['subTitle'=>'セキュリティ情報変更'])
-<p class="text-sm xl:text-base px-2 mb-1 bg-bg rounded-full inline-block">メールアドレス</p>
+<p class="text-lg md:ml-4 ml-2 px-2 mb-2 bg-bg rounded-full inline-block">メールアドレス</p>
 @if (count($errors) > 0)
 @error('email')
 <td style="color: red;">{{$message}}</td>
 @enderror
 @error('email_confirm')
 {{-- TODO:エラーメッセージなぜか赤色にならない 機能的には問題なし--}}
-{{--修正しましたばいてつ--}}
+{{--修正しました ばいてつ--}}
 <p class="text-red-600 inline-block">※メールアドレスが一致していません</p>
 @enderror
 @endif
@@ -34,7 +34,7 @@
         <input type="submit" class="text-black" style="border-radius:4px" value="メールアドレスを変更する">
     </div>
 </form>
-<p class="text-sm xl:text-base px-2 mb-1 bg-bg rounded-full inline-block">パスワード</p>
+<p class="text-lg md:ml-4 ml-2 px-2 mb-2 bg-bg rounded-full inline-block">パスワード</p>
 @if (count($errors) > 0)
 @error('password')
 <td>{{$message}}</td>
@@ -57,26 +57,30 @@
         <input type="submit" class="text-black" style="border-radius:4px" value="パスワードを変更する">
     </div>
 </form>
-@include('components.forMembers.pageSubTitle', ['subTitle'=>'ユーザー情報'])
-<p class="text-xl">ユーザーID: {{$user->id}}</p>
-<p class="text-xl">ユーザー権限ID: {{$user->user_role_id}}</p>
-<p class="text-xl">登録日: {{$user->created_at->format('Y/n/j')}}</p>
+<div class="mb-20">
+    @include('components.forMembers.pageSubTitle', ['subTitle'=>'ユーザー情報'])
+    <p class="text-xl">ユーザーID: {{$user->id}}</p>
+    <p class="text-xl">ユーザー権限ID: {{$user->user_role_id}}</p>
+    <p class="text-xl">登録日: {{$user->created_at->format('Y/n/j')}}</p>
+</div>
 
-@include('components.forMembers.pageSubTitle', ['subTitle'=>'DangerZone'])
-<div class="flex items-center justify-center mb-12">
-    @if($gate->allows('level4~'))
-    <form method="POST" action="/procedure/retire" class="mx-2">
-        @csrf
-        <input type="submit" class="text-white py-2 px-2  " style="background-color: red!important" value="引退する"
-            onclick="retireEvent()">
-    </form>
-    @endif
-    <form method="POST" action="/procedure/withdraw" class="mx-2">
-        @csrf
-        <input type="submit" class="text-white  py-2 px-2 " style="background-color: red!important" value="退部する"
-            onclick="withdrawEvent()">
-    </form>
+<div class="text-center">
+    <h2 class="my-4 font-bold py-2 px-4 border-4 border-red-400 inline-block mx-auto">DangerZone</h2>
+    <div class="flex items-center justify-center mb-12">
+        @if($gate->allows('level4~'))
+        <form method="POST" action="/procedure/retire" class="mx-2">
+            @csrf
+            <input type="submit" class="text-white py-2 px-2  " style="background-color: red!important" value="引退する"
+                onclick="retireEvent()">
+        </form>
+        @endif
+        <form method="POST" action="/procedure/withdraw" class="mx-2">
+            @csrf
+            <input type="submit" class="text-white  py-2 px-2 " style="background-color: red!important" value="退部する"
+                onclick="withdrawEvent()">
+        </form>
 
+    </div>
 </div>
 
 <script>
